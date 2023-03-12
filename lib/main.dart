d9da54431f06be1ae927689051163256445dc612
Wojciech Gajda <wggajda@gmail.com>
2023-03-12 20:58:28 +0100

    API + example

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:openapi/openapi.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final api = Openapi(
+      dio: Dio(BaseOptions(baseUrl: 'https://dionizos-backend-app.azurewebsites.net')),
+      serializers: standardSerializers);
 
   @override
   Widget build(BuildContext context) {
@@ -14,22 +20,36 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Home page with events'),
+      home: MyHomePage(title: 'Home page with events',
+          api: api
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title, required this.api});
   final String title;
+  final Openapi api;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  void strzalDoAPI() async
+  {
+    var res = await widget.api.getEventApi().getEvents();
+    print(res.data);
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
+    strzalDoAPI();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
