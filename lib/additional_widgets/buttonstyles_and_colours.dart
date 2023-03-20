73799ce504f685f8b120ce25d388ed7479c281fc
AW <agataweglik@gmail.com>
2023-03-20 07:44:13 +0100

    modified two files in additional_widgets - date display

--- lib/additional_widgets/buttonstyles_and_colours.dart
+++ lib/additional_widgets/buttonstyles_and_colours.dart
@@ -7,11 +7,14 @@ class PageColor {
   static final singleEvent = Color.fromARGB(255, 192, 192, 255);
   static final appBar = Color.fromARGB(255, 97, 97, 233);
   static final ticket = Color.fromARGB(255, 95, 227, 148);
+  static final divider = Color.fromARGB(255, 149, 149, 254);
 }
 
 class IconsInApp {
-  static IconData dateIcon =
+  static IconData dateIcon1 =
       const IconData(0xf06c8, fontFamily: 'MaterialIcons');
+  static IconData dateIcon0 =
+      const IconData(0xe1b6, fontFamily: 'MaterialIcons');
   static IconData clockIcon =
       const IconData(0xe03a, fontFamily: 'MaterialIcons');
   static IconData freePlacesIcon2 =
